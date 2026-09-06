78b21adf2a8b067da83a60758c4d659c4feb8339
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-09-06 13:43:55 +0530

    Update __init__.py

--- forge/__init__.py
+++ forge/__init__.py
@@ -1,11 +1,3 @@
 # forge/__init__.py
-__version__ = "1.1.6"
+__version__ = "1.3.0"
 __all__ = ["cli", "gui", "core", "templates", "utils", "config"]
-
-# Explicitly import submodules to make PyInstaller see them
-from . import cli
-from . import gui
-from . import core
-from . import templates
-from . import utils
-from . import config
