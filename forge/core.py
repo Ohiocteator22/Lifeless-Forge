01fd3fae592b3b7bba04a9d39e2fb33759128d80
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-09-06 13:40:34 +0530

    Update core.py

--- forge/core.py
+++ forge/core.py
@@ -7,8 +7,8 @@ import lzma
 import shutil
 import tarfile
 from pathlib import Path
-from .utils import format_size, parse_size_string, get_progress_printer
-from .templates import create_pptx_template, create_docx_template, create_xlsx_template
+from forge.utils import format_size, parse_size_string, get_progress_printer
+from forge.templates import create_pptx_template, create_docx_template, create_xlsx_template
 
 # Try to import zstandard
 try:
