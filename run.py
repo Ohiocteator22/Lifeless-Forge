8c7aa08fa7d4e1d4c8b6e306a2c21dfebb4f456f
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-09-06 13:23:18 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-# run.py
 import sys
 import os
-
-# Add the current directory to sys.path so that 'forge' is found
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from forge.__main__ import main
-
 if __name__ == "__main__":
     main()
